ab185edc23bf60a5fef1b81ddd052889c6aa2838
Sophia Koeut <koeutsophia@gmail.com>
2019-10-23 09:44:34 -0700

    deleter

--- drill1.js
+++ drill1.js
@@ -57,7 +57,26 @@ function personMaker() {
         return person;
   }
 
-
-
+  const sampleObj = {
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
+  };
   
+  function keyDeleter(obj) {
+    delete obj.foo
+    delete obj.bar
+    return obj
+  }
+
+  (function testKeyDeleter() {
+    var obj = keyDeleter({
+      foo: 'foo',
+      bar: 'bar',
+      bizz: 'bizz',
+      bang: 'bang',
+    });
+  
+
 
