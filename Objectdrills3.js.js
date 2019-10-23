a019b1c61be5d05b7afd003ae75f6ecbfd9b2e93
Sophia Koeut <koeutsophia@gmail.com>
2019-10-23 14:54:06 -0700

    commit it

--- Objectdrills3.js.js
+++ Objectdrills3.js.js
@@ -8,3 +8,22 @@ const loaf = {
 }
 let finalResult = loaf.hydration();
 console.log(finalResult);
+
+for(let property in newObject)
+
+let hero = data.map(function(data){ 
+
+{ ${data.name} reports to ${data.boss} as ${data.jobTitle};
+} `${data.jobTitle} ${data.name} doesn't report to anybody.` } }); return hero;
+
+let superHeroes = [ 
+    { name: 'Clark Kent', jobTitle:'Reporter', boss: "Bruce Wayne" },
+    { name: 'Bruce Wayne', jobTitle:'Designated BoneBreaker' }, 
+    { name: "Sonic the Hedgehog", jobTitle:"GO FAST!", boss: "Bruce Wayne" }, 
+];
+
+
+let objKeyLength = Object.keys(superHeroes);
+if(Object.keys(objKeyLength).length !== 3){
+    return `${data.jobTitle} ${data.name} doesn't report to anybody.`
+}
