db60e2ddc7a40d781f91e94aee72cd0b9ade560e
Kyle Jensen <kylenjensen19@gmail.com>
2019-10-23 11:47:09 -0700

    finished first drill

--- drill2.js
+++ drill2.js
@@ -1,35 +1,44 @@
 'use strict '
-// function makeStudentsReport(data) {
-    
-// }
-  const studentData = [
-    {
-      name: 'Tim',
-      status: 'Current student',
-      course: 'Biology',
-    },
-    {
-      name: 'Sue',
-      status: 'Withdrawn',
-      course: 'Mathematics',
-    },
-    {
-      name: 'Liz',
-      status: 'On leave',
-      course: 'Computer science',
-    },
+const testData = [
+    { name: 'Jane Doe', grade: 'A' },
+    { name: 'John Dough', grade: 'B' },
+    { name: 'Jill Do', grade: 'A' },
   ];
-  
-  function enrollInSummerSchool(students) {
-    let makeStudent = students.map(function(student){
-        student.status = 'Enrolled in summer school'
-        return student;
-     });
-     console.log(makeStudent);
-     return makeStudent;
-     
+function makeStudentsReport(data) {
+    let studentReport = data.map(function(student){
+        return `${student.name}: ${student.grade}`
+    });
+    return studentReport;
 }
-enrollInSummerSchool(studentData);
+console.log(makeStudentsReport(testData));
+//   const studentData = [
+//     {
+//       name: 'Tim',
+//       status: 'Current student',
+//       course: 'Biology',
+//     },
+//     {
+//       name: 'Sue',
+//       status: 'Withdrawn',
+//       course: 'Mathematics',
+//     },
+//     {
+//       name: 'Liz',
+//       status: 'On leave',
+//       course: 'Computer science',
+//     },
+//   ];
+  
+//   function enrollInSummerSchool(students) {
+//     let makeStudent = students.map(function(student){
+//         student.status = 'Enrolled in summer school'
+//         return student;
+//      });
+//      console.log(makeStudent);
+//      return makeStudent;
+     
+// }
+// enrollInSummerSchool(studentData);
 //   const scratchData = [
 //     { id: 22, foo: 'bar' },
 //     { id: 28, foo: 'bizz' },
