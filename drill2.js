9b91dee91a5546562b82abd17748ac821a046cb9
Kyle Jensen <kylenjensen19@gmail.com>
2019-10-23 12:20:07 -0700

    Finished last drill

--- drill2.js
+++ drill2.js
@@ -1,44 +1,44 @@
 'use strict '
-// const testData = [
-//     { name: 'Jane Doe', grade: 'A' },
-//     { name: 'John Dough', grade: 'B' },
-//     { name: 'Jill Do', grade: 'A' },
-//   ];
-// function makeStudentsReport(data) {
-//     let studentReport = data.map(function(student){
-//         return `${student.name}: ${student.grade}`
-//     });
-//     return studentReport;
-// }
-// console.log(makeStudentsReport(testData));
-//   const studentData = [
-//     {
-//       name: 'Tim',
-//       status: 'Current student',
-//       course: 'Biology',
-//     },
-//     {
-//       name: 'Sue',
-//       status: 'Withdrawn',
-//       course: 'Mathematics',
-//     },
-//     {
-//       name: 'Liz',
-//       status: 'On leave',
-//       course: 'Computer science',
-//     },
-//   ];
+const testData = [
+    { name: 'Jane Doe', grade: 'A' },
+    { name: 'John Dough', grade: 'B' },
+    { name: 'Jill Do', grade: 'A' },
+  ];
+function makeStudentsReport(data) {
+    let studentReport = data.map(function(student){
+        return `${student.name}: ${student.grade}`
+    });
+    return studentReport;
+}
+console.log(makeStudentsReport(testData));
+  const studentData = [
+    {
+      name: 'Tim',
+      status: 'Current student',
+      course: 'Biology',
+    },
+    {
+      name: 'Sue',
+      status: 'Withdrawn',
+      course: 'Mathematics',
+    },
+    {
+      name: 'Liz',
+      status: 'On leave',
+      course: 'Computer science',
+    },
+  ];
   
-//   function enrollInSummerSchool(students) {
-//     let makeStudent = students.map(function(student){
-//         student.status = 'Enrolled in summer school'
-//         return student;
-//      });
-//      console.log(makeStudent);
-//      return makeStudent;
+  function enrollInSummerSchool(students) {
+    let makeStudent = students.map(function(student){
+        student.status = 'Enrolled in summer school'
+        return student;
+     });
+     console.log(makeStudent);
+     return makeStudent;
      
-// }
-// enrollInSummerSchool(studentData);
+}
+enrollInSummerSchool(studentData);
   const scratchData = [
     { id: 22, foo: 'bar' },
     { id: 28, foo: 'bizz' },
@@ -54,24 +54,30 @@
     return item;
   }
   console.log(findById(scratchData, 28));
-//   const objectA = {
-//     id: 2,
-//     name: 'Jane Doe',
-//     age: 34,
-//     city: 'Chicago',
-//   };
+  const objectA = {
+    id: 2,
+    name: 'Jane Doe',
+    age: 34,
+    city: 'Chicago',
+  };
   
-//   // running the function with `objectB` and `expectedKeys`
-//   // should return `false`
-//   const objectB = {
-//     id: 3,
-//     age: 33,
-//     city: 'Peoria',
-//   };
+  // running the function with `objectB` and `expectedKeys`
+  // should return `false`
+  const objectB = {
+    id: 3,
+    age: 33,
+    city: 'Peoria',
+  };
   
-//   const expectedKeys = ['id', 'name', 'age', 'city'];
+  const expectedKeys = ['id', 'name', 'age', 'city'];
   
-//   function validateKeys(object, expectedKeys) {
-//     // your code goes here
+  function validateKeys(object, expectedKeys) {
+   let arrKey = Object.keys(object);
+    if (arrKey.length === expectedKeys.length){
+        return true;
+    }else {
+        return false;
+    }
   
-//   }
+  }
+  console.log(validateKeys(objectB, expectedKeys));
