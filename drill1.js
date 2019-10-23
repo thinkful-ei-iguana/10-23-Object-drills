e5cbfbe335f3e061254fa89ece1f871689a733da
Kyle Jensen <kylenjensen19@gmail.com>
2019-10-23 10:28:04 -0700

    made files for drill2 and drill3

--- drill1.js
+++ drill1.js
@@ -78,5 +78,3 @@ function personMaker() {
       bang: 'bang',
     });
   
-
-
---
+++ drill2.js
@@ -0,0 +1,54 @@
+function makeStudentsReport(data) {
+    
+}
+  const studentData = [
+    {
+      name: 'Tim',
+      status: 'Current student',
+      course: 'Biology',
+    },
+    {
+      name: 'Sue',
+      status: 'Withdrawn',
+      course: 'Mathematics',
+    },
+    {
+      name: 'Liz',
+      status: 'On leave',
+      course: 'Computer science',
+    },
+  ];
+  
+  function enrollInSummerSchool(students) {
+  }
+
+  const scratchData = [
+    { id: 22, foo: 'bar' },
+    { id: 28, foo: 'bizz' },
+    { id: 19, foo: 'bazz' },
+  ];
+  
+  function findById(items, idNum) {
+    // your code here
+  }
+  const objectA = {
+    id: 2,
+    name: 'Jane Doe',
+    age: 34,
+    city: 'Chicago',
+  };
+  
+  // running the function with `objectB` and `expectedKeys`
+  // should return `false`
+  const objectB = {
+    id: 3,
+    age: 33,
+    city: 'Peoria',
+  };
+  
+  const expectedKeys = ['id', 'name', 'age', 'city'];
+  
+  function validateKeys(object, expectedKeys) {
+    // your code goes here
+  
+  }
