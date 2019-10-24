f0c6e03ade184bef8358673b8402b90f7eb479e0
Sophia Koeut <koeutsophia@gmail.com>
2019-10-23 17:42:28 -0700

    decoder

--- Objectdrills3.js.js
+++ Objectdrills3.js.js
@@ -29,7 +29,7 @@
 // }
 // let timeOfMeal = food.meals[3];
 // console.log(timeOfMeal);
-let superHeroes = [
+/*let superHeroes = [
    { name: 'Clark Kent',
     jobTitle:'Reporter',
     boss: "Bruce Wayne"
@@ -57,3 +57,86 @@ function identifyHeroes(data){
 }
 let finalResult = loaf.hydration();
 console.log(finalResult);
+
+function decode (encode){
+
+    let solution = [];
+    let arryString = encode.split(" "); //[ 'craft','block','argon','meter','bells','brown','croon','droop' ]
+    
+    
+    for(let i = 0; i < arryString.length; i++){
+        let word = arryString[i];
+        let firstLetter = word.substring(0, 1);
+    
+        if(firstLetter === 'a') {
+            let convertedLetter = word.charAt(1);
+            solution.push(convertedLetter);
+        }
+    
+        if(firstLetter === 'b') {
+            let convertedLetter = word.charAt(2);
+            solution.push(convertedLetter);
+        }
+    
+        if(firstLetter === 'c') {
+            let convertedLetter = word.charAt(3);
+            solution.push(convertedLetter);
+        }
+    
+        if(firstLetter === 'd') {
+            let convertedLetter = word.charAt(4);
+            solution.push(convertedLetter);
+        }
+    
+    }
+    
+    let finalSolution = solution.join('');*/
+    
+    //return finalSolution;
+
+
+
+    let code = {
+        characterC : 'f',
+        characterB : 'o',
+        characterA : 'r',
+        characterSpace: ' ',
+        characterM : 'l',
+        characterDouble: 'o',
+        characterTrouble: 'o',
+        characterD : 'p',
+    }
+
+function decode (codeData){
+    let arrCode = [];
+    
+
+    for(let property in codeData){
+        let keyValue = codeData[property];
+
+        if(keyValue === 'f'){
+            arrCode.push('f')
+        }
+        if(keyValue === 'o'){
+            arrCode.push('o')
+        }
+        if(keyValue === 'r'){
+            arrCode.push('r')
+        }
+        if(keyValue === 'l'){
+            arrCode.push('l')
+        }
+        if(keyValue === 'p'){
+            arrCode.push('p')
+        }
+        else if(keyValue === ' '){
+            arrCode.push(' ');
+        }
+     
+    }
+    let decodedCode = arrCode.join('');
+    return decodedCode;
+}
+
+let result = decode(code);
+console.log(result);
